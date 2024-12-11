c210fd2ed4f1d571ef4c7c4dab5f79f83f17bbb6
Mubashir <zamir.mubashir@gmail.com>
2024-12-11 23:06:58 +0000

    Central gateway.

---
+++ backend/master_gateway/src/main/java/com/group_component/master_gateway/MasterGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.group_component.master_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterGatewayApplication.class, args);
+	}
+
+}
---
+++ backend/master_gateway/src/test/java/com/group_component/master_gateway/MasterGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group_component.master_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
