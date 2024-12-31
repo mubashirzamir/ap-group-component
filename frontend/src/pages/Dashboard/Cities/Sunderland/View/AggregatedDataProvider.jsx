9c861294a98c3845a216392f8771464b07882301
Muhammad Murtaz Amir Naqvi <mn@mcg.dk>
2024-12-31 12:14:48 +0000

    Selection option enhancement

--- frontend/src/pages/Dashboard/Cities/Sunderland/View/AggregatedDataProvider.jsx
+++ frontend/src/pages/Dashboard/Cities/Sunderland/View/AggregatedDataProvider.jsx
@@ -5,16 +5,12 @@ import SunderlandService from "@/services/SunderlandService.jsx";
 import { genericNetworkError } from "@/helpers/utils.jsx";
 import { REFRESH_INTERVAL_SUNDERLAND } from "@/helpers/constants.jsx";
 
-const useAggregatedData = (selectedTimeRange) => {
+const AggregatedDataProvider = (selectedTimeRange) => {
     // Providers State
     const [providerLoading, setProviderLoading] = useState(false);
     const [providerData, setProviderData] = useState([]);
     const [providerErrored, setProviderErrored] = useState(null);
-  
-    // City State
-    const [cityLoading, setCityLoading] = useState(false);
-    const [cityData, setCityData] = useState(null);
-    const [cityErrored, setCityErrored] = useState(null);
+
   
     const fetchProviderData = async () => {
       // Fetch Providers Data
@@ -32,38 +28,22 @@ const useAggregatedData = (selectedTimeRange) => {
       }
     };
 
-    const fetchCityData = async () => {
-      // Fetch City Data
-      setCityErrored(null);
-      setCityLoading(true);
   
-      try {
-        const cityResponse = await SunderlandService.getAggregatedDataForCity(selectedTimeRange);
-        setCityData(cityResponse);
-      } catch (error) {
-        setCityErrored(true);
-        genericNetworkError(error);
-      } finally {
-        setCityLoading(false);
-      }
-    }
   
     useEffect(() => {
       // Initial fetch on mount and whenever selectedTimeRange changes
       fetchProviderData();
-      fetchCityData();
   
       // Set interval to fetch data periodically using the constant
       const intervalId = setInterval(() => {
         fetchProviderData();
-        fetchCityData();
       }, REFRESH_INTERVAL_SUNDERLAND); // Use the imported constant
   
       // Cleanup: Clear interval on unmount or when selectedTimeRange changes
       return () => clearInterval(intervalId);
     }, [selectedTimeRange]); // Dependency array includes selectedTimeRange
   
-    return { providerData, providerLoading, providerErrored, cityData, cityLoading, cityErrored };
+    return { providerData, providerLoading, providerErrored};
   };
   
-  export default useAggregatedData;
+  export default AggregatedDataProvider;
