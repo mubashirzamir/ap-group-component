d070724eac4b7740bf847706786d81bd68906354
Swapnil Karma <swapnilkarma@gmail.com>
2025-01-13 16:22:42 +0530

    Submission stage 1

--- frontend/src/components/InputIcon/index.jsx
+++
@@ -1,9 +0,0 @@
-const iconStyle = {
-  color: "#bfbfbf",
-};
-
-const InputIcon = ({ Icon }) => {
-  return <Icon style={iconStyle} />;
-};
-
-export default InputIcon;
--- frontend/src/services/UserService.jsx
+++
@@ -1,9 +0,0 @@
-import request from "@/request.js";
-
-const baseUrl = import.meta.env.VITE_AUTH_API_BASE_URL + "/users";
-
-const deleteUser = (email) => {
-  return request.delete(`${baseUrl}/${email}`);
-};
-
-export default { deleteUser };
