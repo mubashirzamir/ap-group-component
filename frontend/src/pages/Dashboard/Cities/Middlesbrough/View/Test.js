547657d22b8bb630253c4ed90dfcb062c1e49904
Muhammad Murtaz Amir Naqvi <mn@mcg.dk>
2025-01-13 13:54:28 +0000

    frontend code

---
+++ frontend/src/pages/Dashboard/Cities/Middlesbrough/View/Test.js
@@ -0,0 +1,418 @@
+
+
+import React from 'react'
+
+
+
+
+
+export default function MiddleData(){
+  const data = [
+    {
+      "ID": 1,
+      "ProviderID": 117,
+      "AverageConsumption": 181.19,
+      "TotalConsumption": 5435.7,
+      "ConsumptionPeriodStart": "2025-11-27",
+      "ConsumptionPeriodEnd": "2025-12-27"
+    },
+    {
+      "ID": 2,
+      "ProviderID": 173,
+      "AverageConsumption": 381.49,
+      "TotalConsumption": 11444.7,
+      "ConsumptionPeriodStart": "2025-07-27",
+      "ConsumptionPeriodEnd": "2025-08-26"
+    },
+    {
+      "ID": 3,
+      "ProviderID": 199,
+      "AverageConsumption": 464.37,
+      "TotalConsumption": 13931.1,
+      "ConsumptionPeriodStart": "2025-08-03",
+      "ConsumptionPeriodEnd": "2025-09-02"
+    },
+    {
+      "ID": 4,
+      "ProviderID": 200,
+      "AverageConsumption": 324.38,
+      "TotalConsumption": 9731.4,
+      "ConsumptionPeriodStart": "2025-04-07",
+      "ConsumptionPeriodEnd": "2025-05-07"
+    },
+    {
+      "ID": 5,
+      "ProviderID": 187,
+      "AverageConsumption": 371.0,
+      "TotalConsumption": 11130.0,
+      "ConsumptionPeriodStart": "2025-01-19",
+      "ConsumptionPeriodEnd": "2025-02-18"
+    },
+    {
+      "ID": 6,
+      "ProviderID": 134,
+      "AverageConsumption": 298.7,
+      "TotalConsumption": 8961.0,
+      "ConsumptionPeriodStart": "2025-03-19",
+      "ConsumptionPeriodEnd": "2025-04-18"
+    },
+    {
+      "ID": 7,
+      "ProviderID": 105,
+      "AverageConsumption": 333.5,
+      "TotalConsumption": 10005.0,
+      "ConsumptionPeriodStart": "2025-11-14",
+      "ConsumptionPeriodEnd": "2025-12-14"
+    },
+    {
+      "ID": 8,
+      "ProviderID": 154,
+      "AverageConsumption": 370.21,
+      "TotalConsumption": 11106.3,
+      "ConsumptionPeriodStart": "2025-06-30",
+      "ConsumptionPeriodEnd": "2025-07-30"
+    },
+    {
+      "ID": 9,
+      "ProviderID": 167,
+      "AverageConsumption": 210.22,
+      "TotalConsumption": 6306.6,
+      "ConsumptionPeriodStart": "2025-08-29",
+      "ConsumptionPeriodEnd": "2025-09-28"
+    },
+    {
+      "ID": 10,
+      "ProviderID": 175,
+      "AverageConsumption": 301.76,
+      "TotalConsumption": 9052.8,
+      "ConsumptionPeriodStart": "2025-07-10",
+      "ConsumptionPeriodEnd": "2025-08-09"
+    },
+    {
+      "ID": 11,
+      "ProviderID": 111,
+      "AverageConsumption": 485.51,
+      "TotalConsumption": 14565.3,
+      "ConsumptionPeriodStart": "2025-11-19",
+      "ConsumptionPeriodEnd": "2025-12-19"
+    },
+    {
+      "ID": 12,
+      "ProviderID": 112,
+      "AverageConsumption": 380.05,
+      "TotalConsumption": 11401.5,
+      "ConsumptionPeriodStart": "2025-10-25",
+      "ConsumptionPeriodEnd": "2025-11-24"
+    },
+    {
+      "ID": 13,
+      "ProviderID": 156,
+      "AverageConsumption": 185.79,
+      "TotalConsumption": 5573.7,
+      "ConsumptionPeriodStart": "2025-06-29",
+      "ConsumptionPeriodEnd": "2025-07-29"
+    },
+    {
+      "ID": 14,
+      "ProviderID": 126,
+      "AverageConsumption": 177.1,
+      "TotalConsumption": 5313.0,
+      "ConsumptionPeriodStart": "2025-08-12",
+      "ConsumptionPeriodEnd": "2025-09-11"
+    },
+    {
+      "ID": 15,
+      "ProviderID": 198,
+      "AverageConsumption": 338.73,
+      "TotalConsumption": 10161.9,
+      "ConsumptionPeriodStart": "2025-11-09",
+      "ConsumptionPeriodEnd": "2025-12-09"
+    },
+    {
+      "ID": 16,
+      "ProviderID": 186,
+      "AverageConsumption": 414.42,
+      "TotalConsumption": 12432.6,
+      "ConsumptionPeriodStart": "2025-09-14",
+      "ConsumptionPeriodEnd": "2025-10-14"
+    },
+    {
+      "ID": 17,
+      "ProviderID": 139,
+      "AverageConsumption": 322.22,
+      "TotalConsumption": 9666.6,
+      "ConsumptionPeriodStart": "2025-03-11",
+      "ConsumptionPeriodEnd": "2025-04-10"
+    },
+    {
+      "ID": 18,
+      "ProviderID": 159,
+      "AverageConsumption": 304.72,
+      "TotalConsumption": 9141.6,
+      "ConsumptionPeriodStart": "2025-07-17",
+      "ConsumptionPeriodEnd": "2025-08-16"
+    },
+    {
+      "ID": 19,
+      "ProviderID": 174,
+      "AverageConsumption": 246.26,
+      "TotalConsumption": 7387.8,
+      "ConsumptionPeriodStart": "2025-02-12",
+      "ConsumptionPeriodEnd": "2025-03-14"
+    },
+    {
+      "ID": 20,
+      "ProviderID": 181,
+      "AverageConsumption": 386.29,
+      "TotalConsumption": 11588.7,
+      "ConsumptionPeriodStart": "2025-10-08",
+      "ConsumptionPeriodEnd": "2025-11-07"
+    },
+    {
+      "ID": 21,
+      "ProviderID": 144,
+      "AverageConsumption": 151.76,
+      "TotalConsumption": 4552.8,
+      "ConsumptionPeriodStart": "2025-07-09",
+      "ConsumptionPeriodEnd": "2025-08-08"
+    },
+    {
+      "ID": 22,
+      "ProviderID": 170,
+      "AverageConsumption": 213.06,
+      "TotalConsumption": 6391.8,
+      "ConsumptionPeriodStart": "2025-04-09",
+      "ConsumptionPeriodEnd": "2025-05-09"
+    },
+    {
+      "ID": 23,
+      "ProviderID": 179,
+      "AverageConsumption": 154.23,
+      "TotalConsumption": 4626.9,
+      "ConsumptionPeriodStart": "2025-09-10",
+      "ConsumptionPeriodEnd": "2025-10-10"
+    },
+    {
+      "ID": 24,
+      "ProviderID": 153,
+      "AverageConsumption": 375.76,
+      "TotalConsumption": 11272.8,
+      "ConsumptionPeriodStart": "2025-09-01",
+      "ConsumptionPeriodEnd": "2025-10-01"
+    },
+    {
+      "ID": 25,
+      "ProviderID": 143,
+      "AverageConsumption": 486.04,
+      "TotalConsumption": 14581.2,
+      "ConsumptionPeriodStart": "2025-01-06",
+      "ConsumptionPeriodEnd": "2025-02-05"
+    },
+    {
+      "ID": 26,
+      "ProviderID": 134,
+      "AverageConsumption": 405.78,
+      "TotalConsumption": 12173.4,
+      "ConsumptionPeriodStart": "2025-05-27",
+      "ConsumptionPeriodEnd": "2025-06-26"
+    },
+    {
+      "ID": 27,
+      "ProviderID": 190,
+      "AverageConsumption": 407.87,
+      "TotalConsumption": 12236.1,
+      "ConsumptionPeriodStart": "2025-11-11",
+      "ConsumptionPeriodEnd": "2025-12-11"
+    },
+    {
+      "ID": 28,
+      "ProviderID": 162,
+      "AverageConsumption": 395.51,
+      "TotalConsumption": 11865.3,
+      "ConsumptionPeriodStart": "2025-08-01",
+      "ConsumptionPeriodEnd": "2025-08-31"
+    },
+    {
+      "ID": 29,
+      "ProviderID": 156,
+      "AverageConsumption": 449.86,
+      "TotalConsumption": 13495.8,
+      "ConsumptionPeriodStart": "2025-04-11",
+      "ConsumptionPeriodEnd": "2025-05-11"
+    },
+    {
+      "ID": 30,
+      "ProviderID": 154,
+      "AverageConsumption": 297.96,
+      "TotalConsumption": 8938.8,
+      "ConsumptionPeriodStart": "2025-08-19",
+      "ConsumptionPeriodEnd": "2025-09-18"
+    },
+    {
+      "ID": 31,
+      "ProviderID": 128,
+      "AverageConsumption": 321.25,
+      "TotalConsumption": 9637.5,
+      "ConsumptionPeriodStart": "2025-06-10",
+      "ConsumptionPeriodEnd": "2025-07-10"
+    },
+    {
+      "ID": 32,
+      "ProviderID": 123,
+      "AverageConsumption": 119.26,
+      "TotalConsumption": 3577.8,
+      "ConsumptionPeriodStart": "2025-02-12",
+      "ConsumptionPeriodEnd": "2025-03-14"
+    },
+    {
+      "ID": 33,
+      "ProviderID": 151,
+      "AverageConsumption": 196.58,
+      "TotalConsumption": 5897.4,
+      "ConsumptionPeriodStart": "2025-02-03",
+      "ConsumptionPeriodEnd": "2025-03-05"
+    },
+    {
+      "ID": 34,
+      "ProviderID": 179,
+      "AverageConsumption": 451.46,
+      "TotalConsumption": 13543.8,
+      "ConsumptionPeriodStart": "2025-08-25",
+      "ConsumptionPeriodEnd": "2025-09-24"
+    },
+    {
+      "ID": 35,
+      "ProviderID": 178,
+      "AverageConsumption": 403.5,
+      "TotalConsumption": 12105.0,
+      "ConsumptionPeriodStart": "2025-06-12",
+      "ConsumptionPeriodEnd": "2025-07-12"
+    },
+    {
+      "ID": 36,
+      "ProviderID": 103,
+      "AverageConsumption": 358.95,
+      "TotalConsumption": 10768.5,
+      "ConsumptionPeriodStart": "2025-06-27",
+      "ConsumptionPeriodEnd": "2025-07-27"
+    },
+    {
+      "ID": 37,
+      "ProviderID": 163,
+      "AverageConsumption": 387.92,
+      "TotalConsumption": 11637.6,
+      "ConsumptionPeriodStart": "2025-06-12",
+      "ConsumptionPeriodEnd": "2025-07-12"
+    },
+    {
+      "ID": 38,
+      "ProviderID": 193,
+      "AverageConsumption": 453.47,
+      "TotalConsumption": 13604.1,
+      "ConsumptionPeriodStart": "2025-07-10",
+      "ConsumptionPeriodEnd": "2025-08-09"
+    },
+    {
+      "ID": 39,
+      "ProviderID": 130,
+      "AverageConsumption": 323.33,
+      "TotalConsumption": 9699.9,
+      "ConsumptionPeriodStart": "2025-07-01",
+      "ConsumptionPeriodEnd": "2025-07-31"
+    },
+    {
+      "ID": 40,
+      "ProviderID": 115,
+      "AverageConsumption": 250.66,
+      "TotalConsumption": 7519.8,
+      "ConsumptionPeriodStart": "2025-08-25",
+      "ConsumptionPeriodEnd": "2025-09-24"
+    },
+    {
+      "ID": 41,
+      "ProviderID": 200,
+      "AverageConsumption": 256.48,
+      "TotalConsumption": 7694.4,
+      "ConsumptionPeriodStart": "2025-08-14",
+      "ConsumptionPeriodEnd": "2025-09-13"
+    },
+    {
+      "ID": 42,
+      "ProviderID": 183,
+      "AverageConsumption": 193.87,
+      "TotalConsumption": 5816.1,
+      "ConsumptionPeriodStart": "2025-05-01",
+      "ConsumptionPeriodEnd": "2025-05-31"
+    },
+    {
+      "ID": 43,
+      "ProviderID": 196,
+      "AverageConsumption": 203.66,
+      "TotalConsumption": 6109.8,
+      "ConsumptionPeriodStart": "2025-06-03",
+      "ConsumptionPeriodEnd": "2025-07-03"
+    },
+    {
+      "ID": 44,
+      "ProviderID": 194,
+      "AverageConsumption": 400.01,
+      "TotalConsumption": 12000.3,
+      "ConsumptionPeriodStart": "2025-05-07",
+      "ConsumptionPeriodEnd": "2025-06-06"
+    },
+    {
+      "ID": 45,
+      "ProviderID": 100,
+      "AverageConsumption": 214.42,
+      "TotalConsumption": 6432.6,
+      "ConsumptionPeriodStart": "2025-02-17",
+      "ConsumptionPeriodEnd": "2025-03-19"
+    },
+    {
+      "ID": 46,
+      "ProviderID": 123,
+      "AverageConsumption": 200.8,
+      "TotalConsumption": 6024.0,
+      "ConsumptionPeriodStart": "2025-07-29",
+      "ConsumptionPeriodEnd": "2025-08-28"
+    },
+    {
+      "ID": 47,
+      "ProviderID": 144,
+      "AverageConsumption": 416.65,
+      "TotalConsumption": 12499.5,
+      "ConsumptionPeriodStart": "2025-11-07",
+      "ConsumptionPeriodEnd": "2025-12-07"
+    },
+    {
+      "ID": 48,
+      "ProviderID": 180,
+      "AverageConsumption": 321.69,
+      "TotalConsumption": 9650.7,
+      "ConsumptionPeriodStart": "2025-09-02",
+      "ConsumptionPeriodEnd": "2025-10-02"
+    },
+    {
+      "ID": 49,
+      "ProviderID": 171,
+      "AverageConsumption": 360.3,
+      "TotalConsumption": 10809.0,
+      "ConsumptionPeriodStart": "2025-05-05",
+      "ConsumptionPeriodEnd": "2025-06-04"
+    },
+    {
+      "ID": 50,
+      "ProviderID": 109,
+      "AverageConsumption": 216.83,
+      "TotalConsumption": 6504.9,
+      "ConsumptionPeriodStart": "2025-05-27",
+      "ConsumptionPeriodEnd": "2023-06-26"
+    }
+  ]
+  function monthlyAverageProviders(){
+    return ''
+  }
+    return data
+
+}
+
--- frontend/src/pages/Dashboard/Cities/Middlesbrough/Visualization/index.jsx
+++ frontend/src/pages/Dashboard/Cities/Middlesbrough/Visualization/index.jsx
@@ -1,13 +1,156 @@
 import DashboardPage from "@/components/DashboardPage/index.jsx";
+import { useEffect, useState } from "react";
+import { Line } from "react-chartjs-2";
+import NewcastleService from "@/services/NewcastleService.jsx";
+import { genericNetworkError, randomColor } from "@/helpers/utils.jsx";
+import DataWrapper from "@/components/DataWrapper/DataWrapper.jsx";
+import { DatePicker } from "antd";
+import { REFRESH_INTERVAl_NEWCASTLE } from "@/helpers/constants.jsx";
+import {
+  CategoryScale,
+  Chart as ChartJS,
+  Legend,
+  LinearScale,
+  LineElement,
+  Title,
+  Tooltip,
+} from "chart.js";
+import dayjs from "dayjs";
+import MiddleData from "../View/Test";
+import { list } from "postcss";
+
+// Register Chart.js components
+ChartJS.register(
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  Title,
+  Tooltip,
+  Legend,
+);
+
+const chartOptions = {
+  plugins: {
+    title: {
+      display: true,
+      text: "Monthly Average Consumption per Provider",
+    },
+    tooltip: {
+      mode: "index",
+      intersect: false,
+    },
+  },
+  scales: {
+    x: {
+      title: {
+        display: true,
+        text: "Month",
+      },
+    },
+    y: {
+      title: {
+        display: true,
+        text: "Average Consumption",
+      },
+    },
+  },
+};
 
 const Visualization = () => {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+  const [errored, setErrored] = useState(false);
+  const [date, setDate] = useState(dayjs());
+
+  const fetchData = () => {
+    setLoading(true);
+    const data = MiddleData();
+    var listDate = [];
+    var listMonthlyAverage = [];
+    data.forEach(element => {
+      var mydate = new Date(element['ConsumptionPeriodEnd']);
+      listDate.push(mydate.getFullYear())
+      listMonthlyAverage.push(element['AverageConsumption'])
+
+    });
+    // console.log(listDate);
+    console.log(listMonthlyAverage);
+    setData(listMonthlyAverage)
+    setLoading(false)
+  };
+
+  useEffect(() => {
+    // Initial fetch on mount
+    fetchData();
+
+    const intervalId = setInterval(() => {
+      fetchData();
+    }, REFRESH_INTERVAl_NEWCASTLE);
+
+    // Cleanup: Clear interval on unmount to prevent memory leaks
+    return () => clearInterval(intervalId);
+  }, [date]); // Include year as a dependency to fetch data when year changes
+
+  const onDateChange = (date) => setDate(date);
+
   return (
     <DashboardPage
       breadcrumbs={[{ title: "Middlesbrough" }, { title: "Visualization" }]}
     >
-      Visualization
+
+      <div className="flex justify-end p-4">
+        <DatePicker
+          disabled={loading}
+          value={date}
+          onChange={onDateChange}
+          picker="year"
+        />
+      </div>
+      <DataWrapper data={data} loading={loading} errored={errored}>
+        <MonthlyConsumptionChart data={data} />
+      </DataWrapper>
     </DashboardPage>
   );
 };
 
 export default Visualization;
+
+
+
+const MonthlyConsumptionChart = ({ data }) => {
+  return <Line data={makeChartData(data)} options={chartOptions} />;
+};
+
+const makeChartData = (data) => {
+  const providers = ["A", "B", "C"];
+  const months = Array.from({ length: 12 }, (_, index) => index + 1);
+
+  // Initialize chart datasets
+  const datasets = providers.map((provider) => {
+    const providerData = data.filter((item) => {
+      // Map providerId to provider letter
+      const providerMap = { 1: "A", 2: "B", 3: "C" };
+      return providerMap[item.providerId] === provider;
+    });
+
+    // Create an array for each month
+    const monthlyConsumption = months.map((month) => {
+      const monthData = providerData.find((item) => item.month === month);
+      return monthData ? monthData.averageConsumption : 0; // Default to 0 if no data for that month
+    });
+
+    return {
+      label: provider,
+      data: monthlyConsumption,
+      borderColor: randomColor(), // Random color for each provider
+      fill: false, // No fill under the line
+      tension: 0.1,
+    };
+  });
+
+  return {
+    labels: months,
+    datasets: datasets,
+  };
+};
+
