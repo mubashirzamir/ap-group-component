83cbff7a8de1c53981864415478f289772816069
Shubh007ox <110490602+Shubh007ox@users.noreply.github.com>
2025-01-13 14:15:40 +0000

    frontend code (#20)
    Co-authored-by: Muhammad Murtaz Amir Naqvi <mn@mcg.dk>

--- frontend/src/pages/Dashboard/Cities/Middlesbrough/View/index.jsx
+++ frontend/src/pages/Dashboard/Cities/Middlesbrough/View/index.jsx
@@ -3,34 +3,58 @@ import { Table } from "antd";
 import { useEffect, useState } from "react";
 import request from "@/request.js";
 import { genericNetworkError } from "@/helpers/utils.jsx";
+import MiddleData from "./Test";
+// import { MongoClient } from "mongo";
+
+
+
+
+
+
 
 const columns = [
   {
-    title: "ID",
-    dataIndex: "id",
-    key: "id",
+    "title": "ID",
+    "dataIndex": "ID",
+    "key": "ID"
   },
   {
-    title: "Title",
-    dataIndex: "title",
-    key: "title",
+    "title": "ProviderID",
+    "dataIndex": "ProviderID",
+    "key": "ProviderID"
   },
   {
-    title: "Body",
-    dataIndex: "body",
-    key: "body",
+    "title": "AverageConsumption",
+    "dataIndex": "AverageConsumption",
+    "key": "AverageConsumption"
   },
-];
+  {
+    "title": "TotalConsumption",
+    "dataIndex": "TotalConsumption",
+    "key": "TotalConsumption"
+  },
+  {
+    "title": "ConsumptionPeriodStart",
+    "dataIndex": "ConsumptionPeriodStart",
+    "key": "ConsumptionPeriodStart"
+  },
+  {
+    "title": "ConsumptionPeriodEnd",
+    "dataIndex": "ConsumptionPeriodEnd",
+    "key": "ConsumptionPeriodEnd"
+  }
+]
 
 const View = () => {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [posts, setPosts] = useState([]);
+
 
   const fetchPosts = () => {
     setLoading(true);
     request
       .get("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => setPosts(response.data))
+      .then((response) => setPosts(MiddleData()))
       .catch(genericNetworkError)
       .finally(() => setLoading(false));
   };
